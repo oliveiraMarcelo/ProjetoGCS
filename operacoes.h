81407a118da396cc365be9e8dfb0f3e338fdc559
Victor Fernandes <victor.hmfd@gmail.com>
2016-09-12 17:20:37 -0300

    initial commit

---
+++ operacoes.h
@@ -0,0 +1,15 @@
+#ifndef OPERACOES_H
+#define OPERACOES_H
+#include <stdlib.h>
+#include <stdio.h>
+
+double soma(double x, double y);
+
+double subtracao(double x, double y);
+
+double divisao(double x, double y);
+
+double multiplicacao(double x, double y);
+
+
+#endif
